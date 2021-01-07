e71dc0b695a3fa4028b41ad4f5f5cb021acf8e1d
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2021-01-07 23:05:59 +0300

    cw8

--- myscript.js
+++ myscript.js
@@ -4,11 +4,54 @@ let board = [
     ['20', '21', '22']
 ]
 
+let you = "O";
 
-
-function press(cellNO) {
+function press(x,y) {
 
     // Do stuff
-    console.log(cellNO)
+
+    console.log(x,y);
+    if (you == "O") {
+        you = "X"
+    }else{
+        you= "O"
+    };
+
+    board[x][y] = you;
+    let game = document.getElementById(`${x}${y}`);
+    game.innerText= you ; 
+
+    if (board[0][0] == you && board[0][1]== you && board[0][2]== you ) {
+    document.getElementById("winner").innerText= `${you} is the winner`;
+    };
+ if (board[1][0] == you && board[1][1]== you && board[1][2]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner`;   
+    };
+    if (board[2][0] == you && board[2][1]== you && board[2][2]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner`  ; 
+    }
+    if (board[0][0] == you && board[1][1]== you && board[2][2]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner` ;  
+    };
+    if (board[0][1] == you && board[1][1]== you && board[2][1]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner` ;  
+    };
+    if (board[0][2] == you && board[1][2]== you && board[2][2]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner` ;  
+    };
+    if (board[0][2] == you && board[1][1]== you && board[2][0]== you)
+    {
+        document.getElementById("winner").innerText= ` ${you} is the winner` ;  
+    };
     
 }
+function newGame(){
+    document.getElementById("winner").innerText="";
+  game.innerText=""
+}
+      
